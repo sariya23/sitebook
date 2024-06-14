e54da25bd5966685c55f9760f5695835b6d670fd
sariya <dayzwaitik123@yandex.ru>
2024-06-14 23:36:59 +0300

    add enum for model

--- books/models.py
+++ books/models.py
@@ -1,13 +1,21 @@
-from django.db import models
-from django.urls import reverse
+from django.db import models  # type: ignore
+from django.urls import reverse  # type: ignore
 
 
 class PublishedBookManager(models.Manager):
     def get_queryset(self) -> models.QuerySet:
-        return super().get_queryset().filter(is_published=1)
-    
+        return super().get_queryset().filter(is_published=Book.PublishStatus.PUBLISHED)
+
 
 class Book(models.Model):
+    class Meta:
+        ordering = ["-rating", "-title"]
+        indexes = [models.Index(fields=["-rating"])]
+
+    class PublishStatus(models.IntegerChoices):
+        DRAFT = (0, "Черновик")
+        PUBLISHED = (1, "Опубликовано")
+
     title = models.CharField(max_length=70)
     description = models.TextField(blank=True)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
@@ -15,22 +23,15 @@ class Book(models.Model):
     rating = models.IntegerField(null=False, default=0)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(
+        choices=PublishStatus.choices, default=PublishStatus.PUBLISHED
+    )
 
     objects = models.Manager()
     published_book = PublishedBookManager()
 
-    class Meta:
-        ordering = ["-rating", "-title"]
-        indexes = [
-            models.Index(fields=["-rating"])
-        ]
-
-
-
     def __str__(self):
         return self.title
 
-    
     def get_absolute_url(self):
         return reverse("book", kwargs={"book_slug": self.slug})
