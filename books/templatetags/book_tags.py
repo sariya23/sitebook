62100c1d90455b8b87176e1bf4b3149c3d942817
sariya <dayzwaitik123@yandex.ru>
2024-06-16 22:35:02 +0300

    Show only rows with rows

--- books/templatetags/book_tags.py
+++ books/templatetags/book_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from ..models import Genre, Tags
 
@@ -7,10 +8,10 @@ register = template.Library()
 
 @register.inclusion_tag("books/list_genres.html")
 def show_genres(genre_selected=0) -> dict[str, list[dict]]:
-    genres = Genre.objects.all()
+    genres = Genre.objects.annotate(total=Count("books")).filter(total__gt=0)
     return {"genres": genres, "genre_selected": genre_selected}
 
 
 @register.inclusion_tag("books/list_tags.html")
 def show_tags():
-    return {"tags": Tags.objects.all()}
+    return {"tags": Tags.objects.annotate(total=Count("tags")).filter(total__gt=0)}
