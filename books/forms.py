f80ae196d769f9829dff4eedc2ca72a2423cbc83
sariya <dayzwaitik123@yandex.ru>
2024-06-17 23:04:43 +0300

    add validator

--- books/forms.py
+++ books/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.validators import MinLengthValidator, ValidationError
 
 from .models import Author, Genre
 
@@ -9,11 +10,19 @@ class AddBookForm(forms.Form):
         widget=forms.TextInput(attrs={"required": True}),
         required=False,
         label="Заголовок",
+        min_length=5,
+        error_messages={
+            "min_length": "Слишком короткий заголовк",
+            "required": "Обязательное поле",
+        },
     )
     description = forms.CharField(
         widget=forms.Textarea(attrs={"required": True, "cols": 50, "rows": 5}),
         required=False,
         label="Описание",
+        validators=[
+            MinLengthValidator(5, message="Минимальное число букв 5"),
+        ],
     )
     is_published = forms.BooleanField(
         widget=forms.CheckboxInput(attrs={"required": True}),
@@ -33,3 +42,11 @@ class AddBookForm(forms.Form):
         label="Автор",
         empty_label="Укажите атвора",
     )
+
+    def clean__title(self):
+        title = self.cleaned_data["title"]
+        allowed_chars = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ -0123456789"
+        if not (set(title) < set(allowed_chars)):
+            raise ValidationError(
+                "Допусимы только русские символы, дефис, цифры и пробел"
+            )
