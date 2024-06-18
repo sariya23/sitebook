29e9ba18bfc85789f5cdce9a6e2c1639a425156e
sariya <dayzwaitik123@yandex.ru>
2024-06-18 22:28:16 +0300

    Use class ciews

--- books/urls.py
+++ books/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="home"),
+    path("", views.BookHome.as_view(), name="home"),
     path("about/", views.about, name="about"),
-    path("show_book/<slug:book_slug>/", views.show_book, name="book"),
-    path("addbook/", views.add_book, name="add_book"),
+    path("show_book/<slug:book_slug>/", views.ShowBook.as_view(), name="book"),
+    path("addbook/", views.AddBook.as_view(), name="add_book"),
     path("contact/", views.contact, name="contact"),
     path("login/", views.login, name="login"),
-    path("genre/<slug:genre_slug>/", views.show_genre, name="genre"),
-    path("tag/<slug:tag_slug>/", views.show_book_by_tag, name="tags"),
+    path("genre/<slug:genre_slug>/", views.BookGenres.as_view(), name="genre"),
+    path("tag/<slug:tag_slug>/", views.BookTags.as_view(), name="tags"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import DetailView, FormView, ListView
 
 from .forms import AddBookForm, UploadFileForm
-from .models import Book, Genre, Tags, UploadFile
+from .models import Book, Tags, UploadFile
 
 menu = [
     {"title": "Home", "url_name": "home"},
@@ -13,14 +14,107 @@ menu = [
 ]
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    context = {
+class BookHome(ListView):
+    template_name = "books/index.html"
+    context_object_name = "books"
+    extra_context = {
         "title": "Home page",
         "menu": menu,
-        "books": Book.published_book.all().select_related("genre"),
         "genre_selected": 0,
     }
-    return render(request, "books/index.html", context)
+
+    def get_queryset(self):
+        return Book.published_book.all().select_related("genre")
+
+
+class AddBook(FormView):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        form = AddBookForm(request.POST)
+
+        data = {
+            "menu": menu,
+            "title": "Добавить книгу",
+            "form": form,
+        }
+        return render(request, "books/add_book.html", data)
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        form = AddBookForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return redirect("home")
+        data = {
+            "menu": menu,
+            "title": "Добавить книгу",
+            "form": form,
+        }
+        return render(request, "books/add_book.html", data)
+
+
+class BookGenres(ListView):
+    template_name = "books/index.html"
+    context_object_name = "books"
+    allow_empty = False
+
+    def get_queryset(self):
+        return Book.objects.filter(
+            genre__slug=self.kwargs["genre_slug"]
+        ).select_related("genre")
+
+    def get_context_data(self, *, object_list=..., **kwargs):
+        context = super().get_context_data(**kwargs)
+        genre = context["books"][0].genre
+        context["title"] = genre.genre
+        context["menu"] = menu
+        context["genre_selected"] = genre.pk
+        return context
+
+
+class BookTags(ListView):
+    template_name = "books/index.html"
+    context_object_name = "books"
+
+    def get_queryset(self):
+        return Book.published_book.filter(
+            tags__slug=self.kwargs["tag_slug"]
+        ).select_related("genre")
+
+    def get_context_data(self, *, object_list=..., **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag = Tags.objects.get(slug=self.kwargs["tag_slug"])
+        context["title"] = tag.tag
+        context["menu"] = menu
+        context["slug_selected"] = None
+        return context
+
+
+class ShowBook(DetailView):
+    template_name = "books/book.html"
+    slug_url_kwarg = "book_slug"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = context["book"].title
+        context["menu"] = menu
+        return context
+
+    def get_object(self, queryset=...):
+        return get_object_or_404(
+            Book.published_book, slug=self.kwargs[self.slug_url_kwarg]
+        )
+
+
+def contact(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Contact with us page")
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Login page")
+
+
+def page_not_found(request: HttpRequest, exception) -> HttpResponseNotFound:
+    return HttpResponseNotFound("<h1>Page not found</h1>")
 
 
 def about(request: HttpRequest) -> HttpResponse:
@@ -34,70 +128,3 @@ def about(request: HttpRequest) -> HttpResponse:
     return render(
         request, "books/about.html", {"title": "About page", "menu": menu, "form": form}
     )
-
-
-def show_book(request: HttpRequest, book_slug: str) -> HttpResponse:
-    book = get_object_or_404(Book, slug=book_slug)
-    data = {
-        "title": book.title,
-        "menu": menu,
-        "book": book,
-        "genre_id": 1,
-    }
-    return render(request, "books/book.html", data)
-
-
-def add_book(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        form = AddBookForm(request.POST, request.FILES)
-        if form.is_valid():
-            print(form.cleaned_data)
-            form.save()
-            return redirect("home")
-    else:
-        form = AddBookForm(request.POST)
-
-    data = {
-        "menu": menu,
-        "title": "Добавить книгу",
-        "form": form,
-    }
-    return render(request, "books/add_book.html", data)
-
-
-def contact(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Contact with us page")
-
-
-def login(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Login page")
-
-
-def show_genre(request: HttpRequest, genre_slug: str) -> HttpResponse:
-    genre = get_object_or_404(Genre, slug=genre_slug)
-    genres = Book.published_book.filter(genre_id=genre.pk).select_related("genre")
-    context = {
-        "title": f"Жанр {genre.genre}",
-        "menu": menu,
-        "books": genres,
-        "genre_selected": genre.pk,
-    }
-    return render(request, "books/index.html", context)
-
-
-def show_book_by_tag(request: HttpRequest, tag_slug: str) -> HttpResponse:
-    tag = get_object_or_404(Tags, slug=tag_slug)
-    books_with_tag = tag.tags.filter(is_published=1).select_related("genre")
-    print(books_with_tag)
-    data = {
-        "title": tag.tag,
-        "menu": menu,
-        "books": books_with_tag,
-        "slug_selected": None,
-    }
-
-    return render(request, "books/index.html", data)
-
-
-def page_not_found(request: HttpRequest, exception) -> HttpResponseNotFound:
-    return HttpResponseNotFound("<h1>Page not found</h1>")
