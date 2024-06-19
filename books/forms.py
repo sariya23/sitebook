15472b358909c8f1ead8579804f5f37cf9d704cd
sariya <dayzwaitik123@yandex.ru>
2024-06-19 18:31:16 +0300

    Order

--- books/forms.py
+++ books/forms.py
@@ -5,13 +5,6 @@ from .models import Author, Book, Genre
 
 
 class AddBookForm(forms.ModelForm):
-    genre = forms.ModelChoiceField(
-        queryset=Genre.objects.all(), empty_label="Укажите жанр"
-    )
-    author = forms.ModelChoiceField(
-        queryset=Author.objects.all(), empty_label="Укажите автора"
-    )
-
     class Meta:
         model = Book
         fields = (
@@ -29,6 +22,15 @@ class AddBookForm(forms.ModelForm):
             "title": forms.TextInput(attrs={"class": "form-input"}),
             "description": forms.Textarea(attrs={"cols": 50, "rows": 4}),
         }
+
+    genre = forms.ModelChoiceField(
+        queryset=Genre.objects.all(), empty_label="Укажите жанр", label="Жанр"
+    )
+    author = forms.ModelChoiceField(
+        queryset=Author.objects.all(),
+        empty_label="Укажите автора",
+        label="Автор",
+    )
 
     def clean_title(self):
         title = self.cleaned_data["title"]
