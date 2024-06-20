e45e98ee5f6b76dc54b11bc369509a4ff6cd0005
sariya <dayzwaitik123@yandex.ru>
2024-06-20 17:16:00 +0300

    Login and logout views

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,16 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
 
 
-class LoginUserForm(forms.Form):
-    username = forms.CharField(
+class LoginUserForm(AuthenticationForm):
+    username = UsernameField(
         label="Логин", widget=forms.TextInput(attrs={"class": "form-input"})
     )
     password = forms.CharField(
         label="Пароль", widget=forms.PasswordInput(attrs={"class": "form-input"})
     )
+
+    class Meta:
+        model = get_user_model()
+        fields = ("username", "password")
--- users/views.py
+++ users/views.py
@@ -1,29 +1,17 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
 from django.urls import reverse
 
 from .forms import LoginUserForm
 
 
-def login_user(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            credentials = form.cleaned_data
-            user = authenticate(
-                request,
-                username=credentials["username"],
-                password=credentials["password"],
-            )
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse("home"))
-    else:
-        form = LoginUserForm()
-    return render(request, "users/login.html", {"form": form})
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = "users/login.html"
+    extra_context = {"title": "Авторизация"}
 
 
 def logout_user(request: HttpRequest) -> HttpResponse:
     logout(request)
-    return HttpResponseRedirect(reverse("users:login_user"))
+    return HttpResponseRedirect(reverse("home"))
