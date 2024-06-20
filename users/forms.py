814c655cfea939955587ea7be8c4de15c855fc6a
sariya <dayzwaitik123@yandex.ru>
2024-06-20 18:07:27 +0300

    Registration of new users

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UsernameField
+from django.core.exceptions import ValidationError
 
 
 class LoginUserForm(AuthenticationForm):
@@ -14,3 +15,43 @@ class LoginUserForm(AuthenticationForm):
     class Meta:
         model = get_user_model()
         fields = ("username", "password")
+
+
+class RegisterUserForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+            "password_confirm",
+        )
+        labels = {
+            "email": "E-mail",
+            "first_name": "Имя",
+            "last_name": "Фамилия",
+        }
+
+    username = UsernameField(label="Логин")
+    password = forms.CharField(
+        max_length=255, label="Пароль", widget=forms.PasswordInput()
+    )
+    password_confirm = forms.CharField(
+        label="Повторите пароль", widget=forms.PasswordInput()
+    )
+
+    def clean_password_confirm(self):
+        clean_data = self.cleaned_data
+
+        if clean_data["password"] != clean_data["password_confirm"]:
+            raise ValidationError("Пароль не совпадают")
+        return clean_data["password"]
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+
+        if get_user_model().objects.filter(email=email).exists():
+            raise ValidationError("Такой email уже существует")
+        return email
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
 
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -15,3 +16,16 @@ class LoginUser(LoginView):
 def logout_user(request: HttpRequest) -> HttpResponse:
     logout(request)
     return HttpResponseRedirect(reverse("home"))
+
+
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            return render(request, "users/register_done.html")
+    else:
+        form = RegisterUserForm()
+    return render(request, "users/register.html", {"form": form})
